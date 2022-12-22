82a3cff744fd45f3c55b27a46c6f38eef6cf76ed
guilhermebranquinho <70035972+guilhermebranquinho@users.noreply.github.com>
2022-12-22 19:32:11 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
 import datetime
-print("RM 343919 - Guilherme Rodrigues Branquinho")
-print(datetime.datetime.now())
+def lambda_handler(event, context):
+    print("RM 343919 - Guilherme Rodrigues Branquinho")
+    print(datetime.datetime.now())
